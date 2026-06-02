c815d5b1277dfa0c9138021db028924ca42339e4
sw02020 <cowper02@naver.com>
2026-06-02 14:36:46 +0900

    fix: 타임아웃 추가

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -71,7 +71,7 @@ class NotificationApiService {
       uri,
       headers: await _getHeaders(),
       body: json.encode({'id': id}),
-    );
+    ).timeout(const Duration(seconds: 10));
 
     if (response.statusCode == 200) {
       final data = json.decode(response.body);
